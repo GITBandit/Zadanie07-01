93ab38ee90496d57bd67d05ec067ba213faf9129
Dominik Jakubowski <dominikjakubowski@gmail.com>
2017-12-13 22:16:05 +0100

    Day converter created.

---
+++ src/CalendarTest.java
@@ -0,0 +1,18 @@
+public class CalendarTest {
+
+    public static void main(String[] args) {
+
+
+        for (int i =1 ; i<=7; i++) {
+            String x;
+            if (i == 1){ x ="st";}
+            else if (i == 2) { x = "nd";}
+            else if (i ==3) { x = "rd";}
+            else x = "th";
+
+            System.out.println("The " + i + x + " day of the week is " + CalendarConverter.convertDayToString(i));
+
+        }
+
+    }
+}
